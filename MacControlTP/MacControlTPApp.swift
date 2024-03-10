18013a8ade780671ed90c4be128e141315f93675
kyle <kylergib@gmail.com>
2024-03-10 13:38:25 -0400

    Initial Commit

---
+++ MacControlTP/MacControlTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacControlTPApp.swift
+//  MacControlTP
+//
+//  Created by kyle on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MacControlTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
